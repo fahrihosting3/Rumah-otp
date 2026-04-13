706b7f4f7313cc49e89a239d462f893fcff66117
fahrihosting3 <stokorkutcomott@gmail.com>
2026-04-13 11:14:09 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,8 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/auth/**/*.{js,ts,jsx,tsx}",
+    "./app/dashboard/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
